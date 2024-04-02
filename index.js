f27a801dbac0cb234fa730c29262ef2471cafa2b
BekmuhammadDev <aripovbekmuhammad2@gmail.com>
2024-04-02 14:09:43 +0500

    lesson

---
+++ index.js
@@ -0,0 +1,83 @@
+// function aniqlash(oyraqami) {
+//     if (oyraqami >=1 && oyraqami <= 3) {
+//         return "bahor";
+//     }else if (oyraqami >= 4 && oyraqami <= 6) {
+//         return "yoz";
+//     }else if (oyraqami >=7 && oyraqami <=9 ) {
+//         return "kuz";
+//     }else if (oyraqami >=10 && oyraqami <=12) {
+//         return"qish";
+//     }else{
+//         return "aniq oy raqami emas"
+//     }
+// }
+// console.log(aniqlash(7));
+
+
+
+// function yoshAniqlash(tugilganYil) {
+//     let hozirgiSana = new Date();
+//     let hozirgiYil = hozirgiSana.getFullYear();
+//     let yosh = hozirgiYil - tugilganYil;
+
+//     if (yosh > 0) {
+//         return yosh;
+//     } else {
+//         return "Noto'g'ri tug'ilgan yil kiritildi.";
+//     }
+// }
+
+// let tugilganYil = prompt("tug'ilgan yilingizni kiriting:");
+// let foydalanuvchiYoshi = yoshAniqlash(tugilganYil);
+
+// console.log(foydalanuvchiYoshi);
+
+
+
+// function aniqlash(son) {
+//     if (son % 2 == 0) {
+//         return "juft";
+//     } else {
+//         return "toq"
+//     }
+// }
+
+
+
+
+// function aniqlash(operator) {
+//     if (99) {
+//         return"uzmobile";
+//     }else if (97) {
+//         return "mobiuz"
+//     }else if (94) {
+//         return"ucel";
+//     }else if (90) {
+//         return"Belline";
+//     }else  {
+//         return "aniq cod emas";
+//     }
+
+// }
+// console.log(aniqlash(99));
+
+
+
+// For1. k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma tuzilsin.
+
+
+// function aniqlash() {
+// let k = +prompt("Istalgan sonni kiriting:")
+// let n = +prompt("Necha marta takrorlansin?")
+
+// if (isNaN(k) || isNaN(n) ||  k === null || n === null) {
+//     console.error("Noto'g'ri kiritish! Raqamlarni kiriting va 'OK' tugmasini bosing.");
+// } else {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(k);
+//     }
+// }
+// }
+
+
+
